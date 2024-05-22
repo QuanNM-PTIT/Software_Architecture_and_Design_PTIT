0bbcacf4671ee5ab786d70b21ae91911eafbf33a
Huyenptt <thanhhuyen99888164@gmail.com>
2024-05-22 19:39:17 +0700

    Fix response verify_token

--- user_service/user_info/views.py
+++ user_service/user_info/views.py
@@ -7,14 +7,16 @@ from user_model.models import User
 from user_model.serializers import UserSerializer
 from .authentication import SafeJWTAuthentication
 
-
 # Create your views here.
 
 class VerifyTokenView(APIView):
     authentication_classes = [SafeJWTAuthentication]
     def post(self, request):
         if request.user:
-            return JsonResponse({'message': 'Token is valid'}, status=status.HTTP_200_OK)
+            account = request.user
+            user = User.objects.get(account__username=account.username)
+            return JsonResponse({"user_id": user.id, "message": "Token is valid"}, status=status.HTTP_200_OK)
+
         return JsonResponse({'message': 'Token is invalid'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class UserInfoView(APIView):
@@ -23,8 +25,9 @@ class UserInfoView(APIView):
         print(request)
         account = request.user
         serializer = UserSerializer(User.objects.get(account__username=account.username))
-        print(serializer.data)
+
         user = {
+            'id': serializer.data['id'],
             'username': serializer.data['account']['username'],
             'email': serializer.data['email'],
             'first_name': serializer.data['fullname']['first_name'],
